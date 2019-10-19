35c77549ae121ce33bce718c7887bda40ed716a9
Nishikant <nishikantktyade@gmail.com>
2019-10-19 20:11:30 +0530

    Added comment

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+# url for user registration
 urlpatterns = [
     path('register', views.register, name='users-register')
 ]
