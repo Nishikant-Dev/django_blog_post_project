6d86eaf14b88cd0cee7a2771860700b32ee011c3
Nishikant <nishikantktyade@gmail.com>
2019-10-20 18:44:59 +0530

    Added routes for default login and logout Added Logout functionality

--- django_project_new/urls.py
+++ django_project_new/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_view
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('users/', include('users.urls')),
+    path('profile/', user_view.profile, name='profile'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/login.html'), name='logout')
-
+    # path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', user_view.logout, name='logout'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-
 from .forms import UserRegistration
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as django_logout
 
 # Create your views here.
 def register(request):
@@ -11,8 +11,18 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account creted for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account creted!!You can login now!')
+            return redirect('login')
     else:
         form = UserRegistration()
     return render(request, 'users/registration.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html');
+
+
+def logout(request):
+    django_logout(request)
+    return redirect('login')
