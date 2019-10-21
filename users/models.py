3b9926515af9ecbc8e232ee189ba84dd44ca66d6
Nishikant <nishikantktyade@gmail.com>
2019-10-21 07:47:48 +0530

    Added Profile model for profile picture which extends User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# as we are using default User Model, for having a profile picture for an User we will extend it with
+# User model with one to one relationship
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
